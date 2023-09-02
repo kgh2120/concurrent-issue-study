92d4cab2b42b9fd13ad6de256e33b8e44de4c626
kyoohyun <kgh2120@naver.com>
2023-09-02 16:45:22 +0900

    [chore] add synchronized keyword's problem

--- src/main/java/com/example/concurrentissue/service/StockService.java
+++ src/main/java/com/example/concurrentissue/service/StockService.java
@@ -4,8 +4,6 @@ import com.example.concurrentissue.domain.Stock;
 import com.example.concurrentissue.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 //@Transactional(readOnly = true)
@@ -15,6 +13,10 @@ public class StockService {
     private final StockRepository stockRepository;
 
 
+    /*
+        synchronized의 문제점 : synchronized는 각 프로세스에서만 동시성 문제를 보장한다.
+        하지만 서버가 여러 대라면, synchronized키워드는 동시성 문제를 보장할 수 없다.
+     */
 //    @Transactional(isolation = Isolation.SERIALIZABLE)
     public synchronized void decrease(Long id, Long quantity) {
         // Stock 조회
