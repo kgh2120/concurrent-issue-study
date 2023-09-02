6edf7973f65764925ad8a03ee120d174308f0afe
kyoohyun <kgh2120@naver.com>
2023-09-02 16:23:45 +0900

    [test] add TC on multi-thread

--- src/test/java/com/example/concurrentissue/service/StockServiceTest.java
+++ src/test/java/com/example/concurrentissue/service/StockServiceTest.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import com.example.concurrentissue.domain.Stock;
 import com.example.concurrentissue.repository.StockRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -41,6 +44,33 @@ class StockServiceTest {
         assertThat(finded.getQuantity()).isEqualTo(99);
         
     
+    }
+    
+    @Test
+    void 동시에_100개의_요청 () throws Exception{
+        //given
+        int threadCount = 100;
+        // ExecutorService는 비동기로 처리하는 작업을 반복문으로 해준다고하네..?
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try{
+                    stockService.decrease(1L,1L);
+                }finally {
+                    latch.countDown();
+                }
+            });
+        }
+        //when
+        latch.await(); // 다른 쓰레드에서 실행되는 작업이 완료될 떄 까지 기다려주는 클래스.
+        //then
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        // 100 - 1 (1*100) = 0
+
+        assertThat(stock.getQuantity()).isEqualTo(0);
+
     }
 
 
