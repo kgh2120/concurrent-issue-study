42dc5bf3890767b639e8cdca382d8444e7bebd1e
kyoohyun <kgh2120@naver.com>
2023-09-02 15:51:52 +0900

    init project

---
+++ src/main/java/com/example/concurrentissue/ConcurrentIssueApplication.java
@@ -0,0 +1,13 @@
+package com.example.concurrentissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentIssueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrentIssueApplication.class, args);
+    }
+
+}
